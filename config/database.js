bfebc03dd454dfa6b9242bb13b57f5218a50f6c5
Your Name <you@example.com>
2024-08-23 21:32:07 +0530

    commit 3

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const dbconnect = async () =>{
     try {
         
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/GAMZY')
+        const conn = await mongoose.connect(process.env.MONGODB)
         console.log('mongodb connected');
     } catch (error) {
         console.log('error connecting to MongoDB',err.message);
